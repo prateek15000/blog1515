cd9b8a55f9eaf441f4f58406bd1d84aecbac68e5
prateek15000 <134774647+prateek15000@users.noreply.github.com>
2024-02-26 23:44:15 +0530

    Initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import classes from './navbar.module.css'
+import { Link } from 'react-router-dom'
+import womanImg from '../../assets/woman.jpg'
+import { useState } from 'react'
+
+const Navbar = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleLogout = () => {
+    // Perform logout actions, such as clearing authentication tokens
+    localStorage.removeItem('accessToken');
+    // You may also need to update your state or perform other cleanup actions
+    setShowModal(false);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Link to='/'>hacker space</Link>
+        </div>
+        <ul className={classes.center}>
+          <li className={classes.listItem}>Home</li>
+          <li className={classes.listItem}>About</li>
+          <li className={classes.listItem}>Contacts</li>
+          <li className={classes.listItem}>Categories</li>
+        </ul>
+        <div className={classes.right}>
+          <img onClick={() => setShowModal(prev => !prev)} src={womanImg} className={classes.img} />
+          {showModal &&
+            <div className={classes.modal}>
+              <Link to='/create'>Create</Link>
+              {/* <span> <Link to='/Login'></Link>Logout</span> */}
+              {/* <Link to='./Login'>Logout</Link> */}
+              <Link to='/Login' onClick={handleLogout}>Logout</Link>
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
